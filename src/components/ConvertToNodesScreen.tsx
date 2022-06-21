7d9e1f6099d9ed98e361a28ebb01ac9dcbd11977
nfonseca1 <nathan.fonseca001@gmail.com>
2022-06-20 22:20:58 -0400

    Convert text can be merged with ctrl + space

--- src/components/ConvertToNodesScreen.tsx
+++ src/components/ConvertToNodesScreen.tsx
@@ -6,7 +6,9 @@ interface Props {
 }
 
 interface State {
-    text: string
+    text: string,
+    ctrlKeyDown: boolean,
+    cursorLocation: number
 }
 
 class ConvertToNodesScreen extends React.Component<Props, State> {
@@ -14,7 +16,9 @@ class ConvertToNodesScreen extends React.Component<Props, State> {
         super(props);
 
         this.state = {
-            text: ''
+            text: '',
+            ctrlKeyDown: false,
+            cursorLocation: 0
         }
 
         this.onTextChange = this.onTextChange.bind(this);
@@ -23,6 +27,10 @@ class ConvertToNodesScreen extends React.Component<Props, State> {
         this.removeEmpties = this.removeEmpties.bind(this);
         this.convert = this.convert.bind(this);
         this.addPeriods = this.addPeriods.bind(this);
+        this.onKeyDown = this.onKeyDown.bind(this);
+        this.onKeyUp = this.onKeyUp.bind(this);
+        this.merge = this.merge.bind(this);
+        this.setCursor = this.setCursor.bind(this);
     }
 
     onTextChange(e: React.ChangeEvent) {
@@ -79,7 +87,7 @@ class ConvertToNodesScreen extends React.Component<Props, State> {
             let newText = state.text;
             newText = newText.split('\n').map(line => {
                 if (line.trim() === '') return line;
-                
+
                 let last = line.trim().slice(-1);
                 if (last === '.' 
                     || last === '?'
@@ -96,6 +104,50 @@ class ConvertToNodesScreen extends React.Component<Props, State> {
             return {
                 text: newText
             }
+        })
+    }
+
+    onKeyDown(e: React.KeyboardEvent) {
+        if (e.code === 'Space' && this.state.ctrlKeyDown) {
+            this.setState({
+                ctrlKeyDown: false
+            }, () => {
+                this.merge();
+            })
+        }
+        else if (e.key === 'Control') {
+            this.setState({
+                ctrlKeyDown: true
+            })
+        }
+    }
+
+    onKeyUp(e: React.KeyboardEvent) {
+        if (e.key === 'Control') {
+            this.setState({
+                ctrlKeyDown: false
+            })
+        }
+    }
+
+    setCursor(e: React.MouseEvent) {
+        this.setState({
+            cursorLocation: (e.target as HTMLTextAreaElement).selectionStart
+        })
+    }
+
+    merge() {
+        this.setState((state) => {
+            let first = state.text.slice(0, state.cursorLocation);
+            let second = state.text.slice(state.cursorLocation);
+            let newText = first + second.trimStart();
+
+            return {
+                text: newText
+            }
+        }, () => {
+            let textarea = document.querySelector('.ConvertToNodesScreen textarea') as HTMLTextAreaElement;
+            textarea.setSelectionRange(this.state.cursorLocation, this.state.cursorLocation);
         })
     }
 
@@ -119,7 +171,9 @@ class ConvertToNodesScreen extends React.Component<Props, State> {
             <div className='ConvertToNodesScreenContainer'>
                 <div className='ConvertToNodesScreenBackground' onClick={this.props.clearScreen}></div>
                 <div className='ConvertToNodesScreen'>
-                    <textarea onChange={this.onTextChange} value={this.state.text}></textarea>
+                    <textarea onChange={this.onTextChange} value={this.state.text}
+                        onKeyUp={this.onKeyUp} onKeyDown={this.onKeyDown}
+                        onClick={this.setCursor}></textarea>
                     <div className='options'>
                         <button onClick={this.removeTimestamps}>Remove Timestamps</button>
                         <button onClick={this.addPeriods}>Add Periods</button>
